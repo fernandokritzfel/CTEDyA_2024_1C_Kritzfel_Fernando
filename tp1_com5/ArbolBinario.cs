1bb1ea4e8956f05abd7c7f0bdbbfc467ea361059
fernandokritzfel <87837307+fernandokritzfel@users.noreply.github.com>
2024-04-06 15:43:32 -0300

    primera version tp1

---
+++ tp1_com5/ArbolBinario.cs
@@ -0,0 +1,113 @@
+﻿using System;
+	
+namespace tp1_com5
+{
+	public class ArbolBinario<T>
+	{
+		
+	
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+	
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public bool esVacioIZQ() {
+			return this.hijoIzquierdo==null ;
+		}
+		public bool esVacioDER() {
+			return this.hijoDerecho==null ;
+		}
+		
+		
+		
+		public void inorden() {
+			
+				
+				// llamada recursiva Hijo IZQ
+				if (!this.getHijoIzquierdo().esVacioIZQ())
+					this.getHijoIzquierdo().inorden();
+				
+				//luego de procesa la raiz
+				Console.WriteLine(this.getDatoRaiz() + " ");
+
+				// llamadas recursiva Hijo DER
+				if (!this.getHijoDerecho().esVacioDER())
+					this.getHijoDerecho().inorden();	
+		}
+		
+		public void preorden() {
+				//primero de procesa la raiz
+				Console.WriteLine(this.getDatoRaiz() + " ");
+				
+				// llamada recursiva Hijo IZQ
+				if (!this.getHijoIzquierdo().esVacioIZQ())
+					this.getHijoIzquierdo().preorden();
+
+				// llamadas recursiva Hijo DER
+				if (!this.getHijoDerecho().esVacioDER())
+					this.getHijoDerecho().preorden();	
+								
+		}
+		
+		public void postorden() {
+			
+				
+				// llamada recursiva Hijo IZQ
+				if (!this.getHijoIzquierdo().esVacioIZQ())
+					this.getHijoIzquierdo().postorden();
+
+				// llamadas recursiva Hijo DER
+				if (!this.getHijoDerecho().esVacioDER())
+					this.getHijoDerecho().postorden();
+				
+				//ultimo se procesa la raiz
+				Console.WriteLine(this.getDatoRaiz() + " ");
+		}
+		
+		public void recorridoPorNiveles() {
+		}
+	
+		public int contarHojas() {
+			return 0;
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+		}
+	}
+}
---
+++ tp1_com5/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: ferna
+ * Fecha: 6/4/2024
+ * Hora: 15:29
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace tp1_com5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ArbolBinario <int> arbolbinarioA = new ArbolBinario<int>(1);
+			
+			ArbolBinario <int> hijoIZQ = new ArbolBinario<int> (2);
+			hijoIZQ.agregarHijoIzquierdo (new ArbolBinario<int>(3));
+			hijoIZQ.agregarHijoDerecho (new ArbolBinario<int> (4));
+			
+			ArbolBinario <int> hijoDER = new ArbolBinario<int> (5);
+			hijoDER.agregarHijoIzquierdo (new ArbolBinario<int> (6));
+			hijoDER.agregarHijoDerecho (new ArbolBinario<int> (7));
+			
+			arbolbinarioA.agregarHijoIzquierdo(hijoIZQ);
+			arbolbinarioA.agregarHijoDerecho(hijoDER);
+			
+			
+			Console.WriteLine("RECORRIDO PREORDEN");
+			arbolbinarioA.preorden();
+			Console.WriteLine();
+			Console.WriteLine();
+			
+			Console.WriteLine("RECORRIDO INORDEN");
+			arbolbinarioA.inorden();
+			Console.WriteLine();
+			Console.WriteLine();
+			
+			Console.WriteLine("RECORRIDO POSORDEN");
+			arbolbinarioA.postorden();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.ReadKey(true);
+		}
+	}
+}
